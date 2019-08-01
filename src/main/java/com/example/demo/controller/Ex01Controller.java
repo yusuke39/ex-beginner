6be831fbf526d3e7966e6b1fa9ba1e86b3bd1336
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-01 16:19:29 +0900

    コントローラー作成、結果画面表示用のHTML作成

--- src/main/java/com/example/demo/controller/Ex01Controller.java
+++ src/main/java/com/example/demo/controller/Ex01Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,12 @@ public class Ex01Controller {
 	@RequestMapping("")
 	public String index() {
 		return "ex01";
+	}
+	
+	@RequestMapping("/input")
+	public String input(String name, Model model) {
+		model.addAttribute("name", name);
+		return "ex01-result";
 	}
 
 }
