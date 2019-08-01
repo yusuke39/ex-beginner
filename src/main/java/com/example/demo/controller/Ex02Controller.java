f04fabf3506c181c46e4d91277862367d7fbee57
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-01 16:56:40 +0900

    計算するコントローラーと結果表示のHTMLの作成

--- src/main/java/com/example/demo/controller/Ex02Controller.java
+++ src/main/java/com/example/demo/controller/Ex02Controller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,15 @@ public class Ex02Controller {
 	@RequestMapping("")
 	public String index() {
 		return "ex02";
+	}
+	
+	@RequestMapping("/calc")
+	public String calc(int num1, int num2, Model model) {
+		int answer = num1 + num2;
+		model.addAttribute("answer", answer);
+		model.addAttribute("num1", num1);
+		model.addAttribute("num2", num2);
+		return "ex02-result";
 	}
 
 }
