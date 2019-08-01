9c41065010bc7cae9d9ebc8e0e9d95980fac98da
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-01 17:41:43 +0900

    計算処理ぼコントローラー&合計金額表示のHTML

--- src/main/java/com/example/demo/controller/Ex03Controller.java
+++ src/main/java/com/example/demo/controller/Ex03Controller.java
@@ -1,16 +1,30 @@
 package com.example.demo.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("ex03")
 public class Ex03Controller {
 	
+	@Autowired
+	private ServletContext application;
+	
 	@RequestMapping("")
 	public String index() {
 		return "ex03";
+	}
+	
+	@RequestMapping("/calc")
+	public String calc(int product1, int product2, int product3) {
+		int allPrice = product1 + product2 + product3;
+		double includeTax = allPrice * 1.08;
+		application.setAttribute("allPrice", allPrice);
+		application.setAttribute("includeTax", (int)includeTax);
+		return "ex03-result";
 	}
 
 }
